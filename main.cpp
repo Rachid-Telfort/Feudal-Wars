e6a55611fe61a780c527d2ac366295fe4afdad61
Rachid Telfort <rachidt1994@gmail.com>
2020-10-04 16:54:18 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<cstdlib>
+#include<application_system.h>
+
+int main()
+{
+    feudal_wars::application_system feudal_wars_system;
+    feudal_wars_system.start();
+
+    return EXIT_SUCCESS;
+}
